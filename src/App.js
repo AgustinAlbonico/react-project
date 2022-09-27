2cf1b093ab2dcfa5d42f4756e47e766b520702a7
Albit0 <agustin_010@live.com>
2022-09-27 00:36:08 -0300

    Creacion de componente warning

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import "./App.css";
-import {Warning} from "./Components/Warning"
-import {Head} from "./Components/Head"
-import {Banner} from "./Components/Banner"
-import {Offers} from "./Components/Offers";
-import {WhyChoose} from "./Components/WhyChoose";
-import {WhoMakes} from "./Components/WhoMakes";
-import {Partner} from "./Components/Partner";
-import {Surprise} from "./Components/Surprise";
-import {Instagram} from "./Components/Instagram";
-import {TellaFriend} from "./Components/TellaFriend";
-import {Foot} from "./Components/Foot";
+
+import { Warning } from "./Components/Warning";
+import { Head } from "./Components/Head";
+import { Banner } from "./Components/Banner";
+import { Offers } from "./Components/Offers";
+import { WhyChoose } from "./Components/WhyChoose";
+import { WhoMakes } from "./Components/WhoMakes";
+import { Partner } from "./Components/Partner";
+import { Surprise } from "./Components/Surprise";
+import { Instagram } from "./Components/Instagram";
+import { TellaFriend } from "./Components/TellaFriend";
+import { Foot } from "./Components/Foot";
 
 function App() {
   return (
     <>
-      <Warning/>
-      <Head/>
-      <Banner/>
-      <Offers/>
-      <WhyChoose/>
-      <WhoMakes/>
-      <Partner/>
-      <Surprise/>
-      <Instagram/>
-      <TellaFriend/>
-      <Foot/>
+      <Warning />
+      <Head />
+      <Banner />
+      <Offers />
+      <WhyChoose />
+      <WhoMakes />
+      <Partner />
+      <Surprise />
+      <Instagram />
+      <TellaFriend />
+      <Foot />
     </>
   );
 }
--- src/Components/Warning/index.jsx
+++ src/Components/Warning/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { GrClose } from "react-icons/gr";
 
 export const Warning = () => {
-  return (
-    <div>Warning</div>
-  )
-}
+  const [warningIsActive, setWarningIsActive] = useState(true);
 
+  return (
+    <section
+      className={`${
+        warningIsActive ? "flex" : "hidden"
+      } justify-between items-center w-full text-black py-2 px-8 bg-blue-400 font-medium`}
+    >
+      <div></div>
+      <div>Lorem ipsum dolor sit amet consectetur.</div>
+      <div className="cursor-pointer" onClick={() => setWarningIsActive(false)}>
+        <GrClose size={20} />
+      </div>
+    </section>
+  );
+};
