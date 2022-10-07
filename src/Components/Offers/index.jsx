7bf3b35650df08d1d93baed2bdf29841fd27746c
luciapozzo <luapozzo@gmail.com>
2022-10-06 21:37:55 -0300

    esqueleto del componente listo

--- src/Components/Offers/index.jsx
+++ src/Components/Offers/index.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { useReducer } from "react";
+import './Offers.css'
+import Product from "./Product";
+import { shoppingInitialState, shoppingReducer } from "../../reducers/reducer";
 
 export const Offers = () => {
-  return <div>Offers</div>;
+
+  const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState);
+  
+  const {products, cart} = state;
+
+  const addToCart = (id) => dispatch ({type:"ADD_TO_CART", payload:id})
+
+ 
+  return <div className="offers-container">
+    <h2>Últimas ofertas</h2>
+    <section className="cards-container">
+    {
+      products.map(product => <Product 
+      key = {product.id}
+      data={product}
+      addToCart={addToCart}/> 
+      )
+    }  
+    </section>
+  </div>
 };
