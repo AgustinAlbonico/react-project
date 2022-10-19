a36f24734669c5141305a01ec7c5e82a41706cc7
csfernandez00 <csfernandez00@gmail.com>
2022-10-19 12:37:28 -0300

    modificando surprise

--- src/Components/Surprise/index.jsx
+++ src/Components/Surprise/index.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
 
 export const Surprise = () => {
-  return (
-    <div>Surprise</div>
-  )
-}
+	return (
+		<div className="w-full bg-gray-50 flex flex-row p-16 gap-5">
+			<div id="image" className="flex-1 px-4 w-full ">
+				<img
+					src="https://t2.tudocdn.net/606944?w=1920"
+					alt=""
+					className="object-cover w-full h-5/5"
+				/>
+			</div>
+			<div
+				id="description"
+				className="flex-1 flex flex-col justify-center gap-7 pr-5"
+			>
+				<h2 className="text-5xl font-semibold">
+					<span className="text-skyblue leading-normal		">Surprise Someone</span>
+					<br />
+					Working from Home
+				</h2>
+				<div className="flex flex-col justify-center gap-5 ">
+					<p className="text-xl font-semibold">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+						corrupti dolorum, quas mollitia, ab, quibusdam iusto dolores
+						perspiciatis quae aut adipisci voluptatibus? Quas, nam ullam porro
+						eos a nisi quaerat!
+					</p>
+					<p className="text-xl font-semibold">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,
+						quisquam. Illo quo voluptatibus.ab, quibusdam iusto dolores
+						perspiciatis quae aut adipisci voluptatibus?
+					</p>
+				</div>
+				<button className="rounded p-3 bg-sky-100 bg-skyblue w-36 text-xl text-white font-semibold">
+					Buy now
+				</button>
+			</div>
+		</div>
+	);
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {},
+		colors: {
+			darkblue: "#2493bf",
+			midblue: "#18bsd9",
+			skyblue: "#05dbf2",
+			salmon: "#f2bea0",
+			white: "#ffffff",
+		},
+	},
+	plugins: [],
+};
