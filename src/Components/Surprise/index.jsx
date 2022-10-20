9139973eed2ab1d773de7927ede39f143f3d74ae
csfernandez00 <csfernandez00@gmail.com>
2022-10-19 22:41:20 -0300

    Hecho responsive

--- src/Components/Surprise/index.jsx
+++ src/Components/Surprise/index.jsx
@@ -3,7 +3,7 @@ import { AiFillGift } from "react-icons/ai";
 
 export const Surprise = () => {
 	return (
-		<div className="w-full bg-gray-50 flex flex-row gap-8 p-16 px-20 justify-center">
+		<div className="w-full bg-gray-50 flex lg:flex-row flex-col gap-8 xl:p-16 xl:px-20  md:p-12  p-4 justify-center">
 			<div id="image" className="flex-1 px-4 pr-5  w-full ">
 				<img
 					src="https://www.yankodesign.com/images/design_news/2021/09/desk-setups/minimal_clean_desk_setups_hero.jpg"
@@ -14,16 +14,16 @@ export const Surprise = () => {
 			</div>
 			<div
 				id="description"
-				className="flex-1 flex flex-col justify-center gap-8 pr-5 "
+				className="flex-1 flex flex-col justify-center  gap-8  px-4 pr-5 "
 			>
-				<h2 className="text-5xl font-semibold">
+				<h2 className="2xl:text-5xl text-4xl  font-semibold">
 					<span className="text-skyblue leading-normal		">
 						Sorprende a un amigo
 					</span>
 					<br />
 					Trabajando desde casa
 				</h2>
-				<div className="flex flex-col justify-center gap-8">
+				<div className="flex flex-col justify-center gap-8 ">
 					<p className="text-xl font-semibold ">
 						¿Conoces a alguien que empezó a trabajar desde casa con una
 						computadora lenta y te gustaria darle una ayuda para que pueda
@@ -36,7 +36,7 @@ export const Surprise = () => {
 						Card.
 					</p>
 				</div>
-				<button className="rounded p-3 gap-2 bg-sky-100 bg-skyblue w-fit text-xl text-white font-semibold flex flex-row items-center justify-between hover:bg-darkblue">
+				<button className="rounded p-3 gap-2 bg-sky-100 bg-skyblue md:w-fit w-full text-xl text-white font-semibold flex flex-row items-center md:justify-between justify-center hover:bg-darkblue">
 					Enviar regalo <AiFillGift />
 				</button>
 			</div>
